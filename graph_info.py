1498c62af23cf7b33172cb37a67139a58aef7c2e
Ayana Murakami <g1720541@is.ocha.ac.jp>
2021-10-04 10:53:12 +0900

    add function

--- graph_info.py
+++ graph_info.py
@@ -5,6 +5,9 @@ def get_object(path):
         object1 = pickle.load(f)
     return object1
 # dict_keys(['ori', 'x_idx', 'x_ridx', 'x', 'pos', 'len', 'bounding_box', 'adj', 'graph', 'connected'])
-# ori:
-def get_node_num(ori):
-    return ori['nodelist']
+# ori
+def calc_meta_node_r(ori):
+    nodelist = ori['nodelist']
+    r = 1
+
+    return r
