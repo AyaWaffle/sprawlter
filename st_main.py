9482a8efaa444987b2ef261f32af49c437d6109b
Ayana Murakami <murakami.ayana3@gmail.com>
2021-11-15 09:53:56 +0900

    パラメータ調整

--- st_main.py
+++ st_main.py
@@ -2,6 +2,12 @@ import streamlit as st
 import glob
 import pandas as pd
 
+st.set_page_config(
+    page_title="Visualize Grarph",
+    # layout="wide",
+)
+graph_imgs = glob.glob('./graph_layout/*')
+
 
 st.title('階層型グラフ表示')
 df = pd.read_csv('./sprawlter_result.csv')
@@ -12,9 +18,23 @@ st.dataframe(df.style.highlight_max(axis=0))
 st.markdown('nnpens: nodeとnodeの重なり,　nepens: nodeとedgeの重なり,　eepens: edgeとedgeの重なり')
 st.markdown('---')
 
-# グラフの表示
-st.subheader("グラフの比較表示")
-graph_imgs = glob.glob('./graph_layout/*')
+
+st.subheader("パラメータ調整")
+st.text('new_result = α × nnpen  +  β × nepen  +  γ × eepen')
+
+# 
+with st.form("パラメータ"):
+    get_param = st.columns(3)
+    param_nnpen = get_param[0].number_input("nnpenの係数", min_value=0.0, max_value=1.0)
+    param_nepen = get_param[1].number_input("nepenの係数", min_value=0.0, max_value=1.0)
+    param_eepen = get_param[2].number_input("eepenの係数", min_value=0.0, max_value=1.0)
+
+    # Submitボタン
+    plot_button = st.form_submit_button('再計算する')
+    if plot_button:
+        new_df = df.copy()
+        new_df.insert(4, 'new_result', param_nnpen * new_df.nnpens + param_nepen * new_df.nepens + param_eepen * new_df.eepens)
+        st.dataframe(new_df.style.highlight_min(axis=0))
 
 # container
 def draw_graph(key):
@@ -25,6 +45,10 @@ def draw_graph(key):
         st.image(graph_path)
     else:
         return st.error('0-99で入力')
+
+
+# グラフの表示
+st.subheader("グラフの比較表示")
 
 # 上の段
 upper_cols = st.columns(2)
